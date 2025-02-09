761f418b43c44a3b98cadfffe4fcf80231f6c489
mohamed tergui <simotergui4@gmail.com>
2025-02-10 00:49:00 +0100

    create the store for  auth User

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -2,5 +2,5 @@
 import { UserState } from './user/user.state'; // Import your user state
 
 export interface AppState {
-  user: UserState; // Add other feature states here if needed
+  user: UserState; 
 }
